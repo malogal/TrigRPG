ca7931d299d43f1738d0a942b8aa19c274242eef
bobbyhead333 <mikeandike277@gmail.com>
2024-02-15 12:59:46 -0600

    NPC Movement Updates
    - Updated the NpcMovement node to work properly
    - This node can be added to NPCs to allow them to move in patterns specified by the code
    - The current patterns implemented are "None" (stands still), "Horizontal" (moves back and forth in a straight horizontal line in both directions from its starting point, to a specified distance), and "Vertical" (same as Horizontal, but up/down instead of left/right)
    - Note that speaking hitboxes will need to be adjusted accordingly for each NPC (that is, the area in which the player can interact with the NPC to start dialogue)

--- scenes/characters/NpcMovement.gd
+++ scenes/characters/NpcMovement.gd
@@ -3,71 +3,96 @@ extends CharacterBody2D
 @export var WALK_SPEED: int = 350
 
 var linear_vel = Vector2()
-@export var facing = "down" # (String, "up", "down", "left", "right")
+var facing = "down" # (String, "up", "down", "left", "right")
 
 var anim = ""
 var new_anim = ""
 
-var start_position = get_parent().get_position # The starting position of the NPC (doesn't change)
-var current_position = start_position # The current position of the NPC (updates every frame)
+@export var move_range = 150 # The distance at which the NPC can move from its starting point (default 50)
+@export_enum("Horizontal", "Vertical", "None") var default_move_type = "None"
 
-@export var move_range = 50 # The distance at which the NPC can move from its starting point (default 50)
-@export var default_move_type = "none";
+var parent
+var start_position
 
 enum { MOVESTATE_IDLE, MOVESTATE_WALKING }
 enum { MOVETYPE_NONE, MOVETYPE_HORIZONTAL, MOVETYPE_VERTICAL }
+enum { MOVEDIR_UP, MOVEDIR_DOWN, MOVEDIR_LEFT, MOVEDIR_RIGHT }
 
 var move_state = MOVESTATE_IDLE
 var move_type = MOVETYPE_NONE
+var move_direction = MOVEDIR_DOWN
 
+func _ready():
+	parent = get_parent()
+	start_position = parent.position # The starting position of the NPC (doesn't change)
+	
+	if default_move_type == "Horizontal":
+		move_direction = MOVEDIR_RIGHT
+	elif default_move_type == "Vertical":
+		move_direction = MOVEDIR_DOWN
+
+#Runs every frame
 func _physics_process(_delta):
 	if Dialogs.active:
 		move_state = MOVESTATE_IDLE
 	else:
+		move_state = MOVESTATE_WALKING
 		match default_move_type:
-			"none":
+			"None":
 				move_type = MOVETYPE_NONE
-			"horizontal":
+			"Horizontal":
 				move_type = MOVETYPE_HORIZONTAL
-			"vertical":
+			"Vertical":
 				move_type = MOVETYPE_VERTICAL
 	
 	if move_type != MOVETYPE_NONE:
 		match move_state:
-			MOVESTATE_IDLE:
+			MOVESTATE_IDLE: #If the NPC is idle, make their idle animation play in the direction they are facing
 				new_anim = "idle_" + facing
 			MOVESTATE_WALKING:
-				current_position = get_parent().get_position;
-				
 				match move_type: #If the NPC moves vertically/horizontally, they will change directions when they have exceeded their movement range
 					MOVETYPE_HORIZONTAL:
-						if facing == "right" and current_position.x > (start_position.x + move_range):
-							facing = "left"
-						if facing == "left" and current_position.x < (start_position.x - move_range):
-							facing = "right"
+						if move_direction == MOVEDIR_RIGHT:
+							if parent.position.x > (start_position.x + move_range):
+								facing = "left"
+								move_direction = MOVEDIR_LEFT
+							else:
+								facing = "right"
+						elif move_direction == MOVEDIR_LEFT:
+							if parent.position.x < (start_position.x - move_range):
+								facing = "right"
+								move_direction = MOVEDIR_RIGHT
+							else:
+								facing = "left"
 					MOVETYPE_VERTICAL:
-						if facing == "down" and current_position.y > (start_position.y + move_range):
-							facing = "up"
-						if facing == "up" and current_position.y < (start_position.y - move_range):
-							facing = "down"
+						if move_direction == MOVEDIR_DOWN:
+							if parent.position.y > (start_position.y + move_range):
+								facing = "up"
+								move_direction = MOVEDIR_UP
+							else:
+								facing = "down"
+						elif move_direction == MOVEDIR_UP:
+							if parent.position.y < (start_position.y - move_range):
+								facing = "down"
+								move_direction = MOVEDIR_DOWN
+							else:
+								facing = "up"
 				
-				get_parent().set_velocity(linear_vel)
-				get_parent().move_and_slide()
-				linear_vel = get_parent().velocity
-				
+				linear_vel = velocity
 				var target_speed = Vector2()
 				
-				if facing == "down":
-					target_speed += Vector2.DOWN
-				if facing == "left":
-					target_speed += Vector2.LEFT
-				if facing == "right":
-					target_speed += Vector2.RIGHT
-				if facing == "up":
-					target_speed += Vector2.UP
+				match move_direction:
+					MOVEDIR_DOWN:
+						target_speed += Vector2.DOWN
+					MOVEDIR_LEFT:
+						target_speed += Vector2.LEFT
+					MOVEDIR_RIGHT:
+						target_speed += Vector2.RIGHT
+					MOVEDIR_UP:
+						target_speed += Vector2.UP
 				
-				target_speed *= WALK_SPEED
-				linear_vel = linear_vel.lerp(target_speed, 0.9)
+				target_speed *= (WALK_SPEED / 200.0)
+				parent.position += target_speed
 				
 				new_anim = ""
 				if abs(linear_vel.x) > abs(linear_vel.y):
