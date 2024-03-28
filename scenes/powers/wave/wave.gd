1a2d32e924483c3e751d614581da0efb3a58f636
True F Martin <truefmartin@gmail.com>
2024-03-27 21:23:49 -0500

    Fix wave after resize

--- scenes/powers/wave/wave.gd
+++ scenes/powers/wave/wave.gd
@@ -3,7 +3,7 @@ extends Node2D
 var speed: int
 var degree: int
 
-const rotate_speed = 50
+const rotate_speed = 350
 var current_rotation = 0
 var vec: Vector2
 var target: Vector2
@@ -11,30 +11,23 @@ var target: Vector2
 signal hit 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_wave({})
 	pass # Replace with function body.
 
 # Wave properties, default values
-var amplitude: float = 200 # Height of the wave
-var frequency: float = 0.01 # How many waves within a certain distance
+var amplitude: float = 450 # Height of the wave
+var frequency: float = 0.009 # How many waves within a certain distance
 var is_sine_wave: bool = true # True for sine, false for cosine
-var wave_color_pos: Color = Color(0.00000100867101, 0.5642226934433, 0.51764661073685, 0.5)
-var wave_color_neg: Color = Color(0.79184025526047, 0.31729644536972, 0.10155173391104, 0.5)
+var wave_color_pos: Color = Color(0.00000100867101, 0.5642226934433, 0.51764661073685, 0.7)
+var wave_color_neg: Color = Color(0.79184025526047, 0.31729644536972, 0.10155173391104, 0.7)
 var is_horizontal: bool = true
 
 var wave_points_positive: PackedVector2Array 
 var wave_points_negative: PackedVector2Array 
 
-var debug_i = 0
-func _process(delta: float):
-	debug_i+= 1
-	if debug_i % 50 == 0:
-		print_debug("global: ", get_global_mouse_position())
-		print_debug("local: ", get_local_mouse_position())
 
 func _draw():
-	draw_polyline(wave_points_positive, wave_color_pos, 2, true)
-	draw_polyline(wave_points_negative, wave_color_neg, 2, true)
+	draw_polyline(wave_points_positive, wave_color_pos, 1, true)
+	draw_polyline(wave_points_negative, wave_color_neg, 1, true)
 	
 func fill_array():
 	var start_pos = Vector2(0,0) 
@@ -45,11 +38,11 @@ func fill_array():
 	if is_horizontal:
 		direction = Vector2(1, 0)
 		# Screen width (halved)
-		wave_length = 160
+		wave_length = 850
 	else:
 		direction = Vector2(0, 1)
 		# Screen height (halved)
-		wave_length = 240
+		wave_length = 450
 	set_points(start_pos, 0, -wave_length, direction, wave_points_negative)
 	set_points(start_pos, 0, wave_length, direction, wave_points_positive)
 		
@@ -58,7 +51,7 @@ func set_points(start_pos: Vector2, first: int, last: int, direction: Vector2, a
 	var step = 1 if first < last else -1
 	for i in range(first, last, step):
 		var displacement = direction.normalized() * i
-		var wave_offset: float = (sin(frequency * i)  if is_sine_wave else cos(frequency * i)) * amplitude
+		var wave_offset: float = (sin(frequency * i) if is_sine_wave else cos(frequency * i)) * amplitude
 		var current_point: Vector2
 		
 		if is_horizontal:
@@ -66,7 +59,7 @@ func set_points(start_pos: Vector2, first: int, last: int, direction: Vector2, a
 		else:
 			current_point = start_pos + Vector2(wave_offset, displacement.y)
 		# Skip first line which would draw straight from player to begining of wave
-		arr.append(current_point)
+		arr.append(current_point*.2)
 
 # Create a new wave, multiply default stats by passed in dictionary
 func new_wave(specs: Dictionary):
