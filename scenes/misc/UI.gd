420ba9ae6e896f6fa5e6e5daba0a47df1d75f347
True F Martin <truefmartin@gmail.com>
2024-04-05 23:49:39 -0500

    Clean levels and interface into scenes.
    Outside will hold the level and the InGameInterfaces.
    
    * The InGameInterfaces will hold the audio, the pause menu, and the in-game-ui.
    * The level will hold the character and the 'level' scenes.
        * the 'level' scenes are grouped together into a single scene, for example CampLevel.tscn. This will have children such as the tile map, the npcs, ect.
    
    Important things:
    The player is at the same level as the 'CampLevel' (which is not the level node).
    The level node and the in-game interface are the only two direct children of the Outside.tscn.

--- scenes/misc/UI.gd
+++ scenes/misc/UI.gd
@@ -4,12 +4,10 @@ var player : Player = null
 func _ready():
 	# Try to get the player node. If null wait till next frame, rinse, repeat.
 	while (player == null):
-		var player_group = get_tree().get_nodes_in_group("player")
-		if not player_group.is_empty():
-			player = player_group.pop_front()
-		else:
-			await get_tree().idle_frame
-	
+		player = get_tree().get_first_node_in_group("player")
+		if player == null:
+			printerr("No player found in scene")
+			return
 	player.health_changed.connect($Healthbar._on_health_changed)
 	$Healthbar._on_health_changed(player.hitpoints)
 	$BottomUi.assign_player(player)
