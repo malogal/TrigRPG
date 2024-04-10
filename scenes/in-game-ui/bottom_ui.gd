a0aed9be408e01baf366e09fcfdf4b9f7e2dc571
True F Martin <truefmartin@gmail.com>
2024-04-10 16:10:16 -0500

    Add invisible physics tilemap for quick boundaries

--- scenes/in-game-ui/bottom_ui.gd
+++ scenes/in-game-ui/bottom_ui.gd
@@ -25,37 +25,37 @@ func _process(_delta: float) -> void:
 func _on_pie_changed(pie_amount: Angle):
 	var rads: String = pie_amount.get_rich_str_rad()
 	var deg: String = pie_amount.get_str_deg()
-	$HBoxContainer/PieSection/HBoxContainer/PieNumberRad.text = "[center][color=Crimson]" + rads + "[/color][/center]"
-	$HBoxContainer/PieSection/HBoxContainer/PieNumberDeg.text = "[center][color=Crimson]" + deg + "º[/color][/center]"
-	var num_frames: int = $HBoxContainer/PieSection/HBoxContainer2/PieSprite.texture.frames
-	$HBoxContainer/PieSection/HBoxContainer2/CircleSegment.shade_fraction = pie_amount.rads
+	$Panel/HBoxContainer/PieSection/HBoxContainer/PieNumberRad.text = "[center][color=Crimson]" + rads + "[/color][/center]"
+	$Panel/HBoxContainer/PieSection/HBoxContainer/PieNumberDeg.text = "[center][color=Crimson]" + deg + "º[/color][/center]"
+	var num_frames: int = $Panel/HBoxContainer/PieSection/HBoxContainer2/PieSprite.texture.frames
+	$Panel/HBoxContainer/PieSection/HBoxContainer2/CircleSegment.shade_fraction = pie_amount.rads
 	
 	var increment: float = 2 * PI / ( num_frames)
 	# Set amount of pie to show based on current throw amount  (frame 0 is lowest)
 	for i in range(0, num_frames):
 		if pie_amount.rads <= (i+1) * increment:
-			$HBoxContainer/PieSection/HBoxContainer2/PieSprite.texture.current_frame = i
+			$Panel/HBoxContainer/PieSection/HBoxContainer2/PieSprite.texture.current_frame = i
 			return
 	# If frame was not set above, set it to max
-	$HBoxContainer/PieSection/PieSprite.texture.current_frame = num_frames - 1
+	$Panel/HBoxContainer/PieSection/PieSprite.texture.current_frame = num_frames - 1
 
 func _on_pie_available(is_available: bool):
 	if is_available:
-		$HBoxContainer/PieSection/HBoxContainer2/PieSprite.modulate = Color8(255,255,255)
-		$HBoxContainer/Cooldowns/Buttons/ClickButton.button_pressed = false
+		$Panel/HBoxContainer/PieSection/HBoxContainer2/PieSprite.modulate = Color8(255,255,255)
+		$Panel/HBoxContainer/Cooldowns/Buttons/ClickButton.button_pressed = false
 	else:
-		$HBoxContainer/PieSection/HBoxContainer2/PieSprite.modulate = Color8(92,92,92)
-		$HBoxContainer/Cooldowns/Buttons/ClickButton.button_pressed = true
+		$Panel/HBoxContainer/PieSection/HBoxContainer2/PieSprite.modulate = Color8(92,92,92)
+		$Panel/HBoxContainer/Cooldowns/Buttons/ClickButton.button_pressed = true
 
 func _on_wave_available(is_available: bool):
 	if is_available:
-		$HBoxContainer/Cooldowns/Buttons/FBtutton.button_pressed = false
+		$Panel/HBoxContainer/Cooldowns/Buttons/FBtutton.button_pressed = false
 	else:
-		$HBoxContainer/Cooldowns/Buttons/FBtutton.button_pressed = true
+		$Panel/HBoxContainer/Cooldowns/Buttons/FBtutton.button_pressed = true
 
 func _on_teleport_available(is_available: bool):
 	if is_available:
-		$HBoxContainer/Cooldowns/Buttons/TabButton.button_pressed = false
+		$Panel/HBoxContainer/Cooldowns/Buttons/TabButton.button_pressed = false
 	else:
-		$HBoxContainer/Cooldowns/Buttons/TabButton.button_pressed = true
+		$Panel/HBoxContainer/Cooldowns/Buttons/TabButton.button_pressed = true
 
