6594df42b7232a17e8c283f819c3bc0ce806dc23
malogal <malogal@uark.edu>
2024-04-15 12:51:34 -0500

    minigames finished sans art
    the minigames are fully functional and ready to be implemented. add the triglock and the correct/incorrect transformations and they will work. smack anything with a pie to reset it

--- scenes/minigames/trig_lock.gd
+++ scenes/minigames/trig_lock.gd
@@ -8,7 +8,7 @@ var transformations = []
 
 enum { SIN, COS, TAN, SEC, CSC, COT }
 @export var type := SIN
-@export var frequency := 1
+@export var frequency := 2
 @export var h_shift := 0
 @export var amplitude := 1
 @export var v_shift := 0
@@ -31,37 +31,70 @@ func trig_func(theta:float):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Current.default_color = Color(1,0,0,1)
-	$Current.width = 2
+	$Current.width = 1
 	$Goal.default_color = Color(0,1,0,1)
-	$Goal.width = 2
+	$Goal.width = 1
+	$xaxis.default_color = Color(0,0,0,1)
+	$xaxis.width = 1
+	$yaxis.default_color = Color(0,0,0,1)
+	$yaxis.width = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$yaxis.add_point(Vector2(0,-15))
+	$yaxis.add_point(Vector2(0,15))
+	$xaxis.add_point(Vector2(-80,0))
+	$xaxis.add_point(Vector2(80,0))
+	queue_redraw()
 
 
 func _draw():
+	#for simplification, also figure out if successful
+	success = true
 	$Current.clear_points()
+	$Goal.clear_points()
 	for x in range(-80,80):
+		#current
 		var y = TAU/80*x
 		for t in transformations:
-			if(t.type==FREQUENCY or t.type==SHIFT_UD):
+			if(t.type==FREQUENCY or t.type==SHIFT_LR):
 				y = t.evaluate(y)
 		y = trig_func(y)
 		for t in transformations:
-			if(t.type==AMPLITUDE or t.type==SHIFT_LR):
+			if(t.type==AMPLITUDE or t.type==SHIFT_UD):
 				y = t.evaluate(y)
 		y *= -15
+		var current = y
 		$Current.add_point(Vector2(x,y))
-
-
+		#goal
+		y = TAU/80*x
+		y *= frequency
+		y -= h_shift
+		y = trig_func(y)
+		y *= amplitude
+		y += v_shift
+		y *= -15
+		var goal = y
+		$Goal.add_point(Vector2(x,y))
+		if abs(current-goal)>0.01:
+			success = false
 
 
 
 func _on_body_entered(body):
-	var trans = body.get_parent()
-	print("agsdfadasfg")
+	print("transformation hit by")
+	print(body)
 	if body.is_in_group("transformation"):
 		transformations.append(body)
-		body.reset()
+		body.remove()
+
+
+func _on_hurtbox_body_entered(body):
+	print("transformation hit by")
+	print(body)
+	if body.is_in_group("pie"):
+		print("pie")
+		for t in transformations:
+			t.reset()
+		transformations = []
--- scenes/minigames/unit_circle.gd
+++ scenes/minigames/unit_circle.gd
@@ -6,7 +6,7 @@ const AngleClass = preload("res://misc-utility/Angle.gd")
 @export var goal_value: float = 0.5
 var success = false
 var angle = AngleClass.new(0)
-var radius = 80
+var radius = 154
 
 var minAngle = 0
 var maxAngle = PI/2
@@ -31,7 +31,7 @@ func evaluate_return():
 		return_value = 1./sin(theta)
 	elif type==TrigFunc.COT and sin(theta!=0):
 		return_value = cos(theta)/sin(theta)
-	success = abs(return_value-goal_value)<0.00001
+	success = abs(return_value-goal_value)<0.0001
 
 func type_to_str():
 	if type==TrigFunc.SIN:
@@ -53,6 +53,16 @@ func _ready():
 	detection = $EdgeArea
 	evaluate_return()
 	$TextureRect/Label.text = type_to_str()+"(θ)=0.5"
+	$sin.default_color = Color(1,0,0,1)
+	$sin.width = 2
+	$cos.default_color = Color(0,0,1,1)
+	$cos.width = 2
+	$arc.default_color = Color(0,0,0,1)
+	$arc.width = 1
+	$arc.clear_points()
+	for t in range(-1,12):
+		var theta = PI*t/20
+		$arc.add_point(Vector2(radius*cos(theta),-radius*sin(theta)))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -60,13 +70,19 @@ func _process(delta):
 	angle.rads = -$EdgeArea.rotation
 	angle.round(5,true)
 	evaluate_return()
-	#$Gate.disabled = success
 	if success:
 		$TextureRect/Label.label_settings.font_color = Color(0, 0.61960786581039, 0, 0.88627451658249)
-		#$Gate/AnimatedSprite2D.visible = false
 	else:
 		$TextureRect/Label.label_settings.font_color = Color(1, 0.1176470592618, 0.37647059559822, 0.8941176533699)
-		#$Gate/AnimatedSprite2D.visible = true
+	queue_redraw()
+
+func _draw():
+	$sin.clear_points()
+	$sin.add_point(Vector2(radius*cos($EdgeArea.rotation),0))
+	$sin.add_point(Vector2(radius*cos($EdgeArea.rotation),radius*sin($EdgeArea.rotation)))
+	$cos.clear_points()
+	$cos.add_point(Vector2(0,0))
+	$cos.add_point(Vector2(radius*cos($EdgeArea.rotation),0))
 
 #func _on_edge_area_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	#if body.is_in_group("player"):
